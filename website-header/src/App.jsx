133afcabe2be96d0b59d99d7fc274a8dbc5043e5
STS-tamil <senthamil1802@gmail.com>
2025-10-08 16:23:14 +0530

    version1

---
+++ website-header/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import {useEffect, useState} from 'react'
+import Background from './Components/Background/Background'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+function App() {
+  let heroData = [
+    {text1:"Dive into",text2:"What you love"},
+    {text1:"Indulge",text2:"Your Passions"},
+    {text1:"Give in to",text2:"your Passions"}
+  ]
+
+  const [heroCount,setHeroCount] = useState(2)
+  const [playStatus,setPlayStatus] = useState(false)
+
+  useEffect (() =>{
+    setInterval(()=>{
+      setHeroCount((count)=>{return count ===2?0:count+1})
+    },3000);
+  },[])
+
+  return (
+    <>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar />
+      <Hero setPlayStatus={setPlayStatus} heroData={heroData[heroCount]} heroCount={heroCount} setHeroCount={setHeroCount} playStatus={playStatus}/>
+    </>
+  )
+}
+
+export default App
